bef974e55b5bffa065bca770b11496803e0f35de
LucasLeculier <102011516+LucasLeculier@users.noreply.github.com>
2023-12-21 10:45:13 +0100

    Fichier python de formatage du dataSet

---
+++ hackaton.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 21 10:33:56 2023
+
+@author: lluca
+"""
+import pandas as pd
+
+file_path = 'Consolidated_Village_Data.xlsx'
+xls = pd.ExcelFile(file_path)
+
