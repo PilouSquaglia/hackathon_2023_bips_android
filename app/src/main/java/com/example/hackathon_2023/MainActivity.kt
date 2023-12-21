b14b611bff561aa9923ebc97de468bd8b9843295
pilou <pl.squaglia@gmail.com>
2023-12-21 10:18:27 +0100

    add maps activity

--- app/src/main/java/com/example/hackathon_2023/MainActivity.kt
+++ app/src/main/java/com/example/hackathon_2023/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.hackathon_2023
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mapView: MapView
+    private lateinit var googleMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mapView = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onMapReady(gMap: GoogleMap) {
+        googleMap = gMap
+
+        // Centrer la carte sur Bastia
+        val bastia = LatLng(42.7028, 9.4489)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bastia, 15f))
     }
 }
