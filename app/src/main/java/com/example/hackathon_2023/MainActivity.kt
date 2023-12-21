f66fe3f02174b46c4ac6b9c3251d338c7d7a1e4d
pilou <pl.squaglia@gmail.com>
2023-12-21 22:08:17 +0100

    add fix routes

--- app/src/main/java/com/example/hackathon_2023/MainActivity.kt
+++ app/src/main/java/com/example/hackathon_2023/MainActivity.kt
@@ -20,16 +20,16 @@ import com.google.maps.DirectionsApi
 import com.google.maps.GeoApiContext
 import com.google.maps.model.DirectionsResult
 import com.google.maps.model.TravelMode
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import com.google.maps.android.PolyUtil
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mapView: MapView
     private lateinit var googleMap: GoogleMap
-    private lateinit var polyline: Polyline
 
     private lateinit var btnZoomIn: Button
     private lateinit var btnZoomOut: Button
@@ -77,50 +77,55 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         this.googleMap = googleMap
 
+        // Load points from JSON
+        val points = loadPointsFromJson()
+
+        for (point in points) {
+            addMarker(point)
+        }
+
+        println("######################")
+        println(points)
+
         // Example coordinates for the route
-        val origin = LatLng(42.59513880243619, 9.32103294032586)
-        val destination = LatLng(42.59584862940876, 9.328090874946884)
+        val origin = points[0]
+        val destination = points[points.size - 1]
 
-        val originalIcon = BitmapFactory.decodeResource(resources, R.drawable.trash_bin)
-
-        val resizedIcon = Bitmap.createScaledBitmap(originalIcon, 100, 100, false)
-
-        val customIcon = BitmapDescriptorFactory.fromBitmap(resizedIcon)
-
-        googleMap.addMarker(
-            MarkerOptions()
-                .position(origin)
-                .title("Marker Title")
-                .snippet("Marker Description")
-                .icon(customIcon)
-        )
-
-        // Add markers for the points
-        googleMap.addMarker(MarkerOptions().position(destination).title("Destination"))
+        // Draw the route
+        drawRoute(points)
 
         // Move the camera to the origin
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(origin, 14f))
-
-        // Draw the route
-        getDirections(origin, destination)
     }
 
-    private fun getDirections(origin: LatLng, destination: LatLng) {
-        val apiKey = "AIzaSyBHYb9-kS4yMnYYDgdovD2EUpr8G_iFexQ" // Replace with your actual API key
+    private fun drawRoute(points: List<LatLng>) {
+        for (i in 0 until points.size - 1) {
+            val origin = points[i]
+            val destination = points[i + 1]
+
+            // Get directions between the current and next point
+            val directions = getDirections(origin, destination)
+
+            // Draw the detailed route on the map
+            drawDetailedRoute(directions)
+        }
+    }
+
+    private fun getDirections(origin: LatLng, destination: LatLng): DirectionsResult {
+        val apiKey = "AIzaSyBHYb9-kS4yMnYYDgdovD2EUpr8G_iFexQ"
         val context = GeoApiContext.Builder().apiKey(apiKey).build()
 
-        CoroutineScope(Dispatchers.Main).launch {
-            try {
-                val directionsResult = DirectionsApi.newRequest(context)
-                    .mode(TravelMode.DRIVING)
-                    .origin(com.google.maps.model.LatLng(origin.latitude, origin.longitude))
-                    .destination(com.google.maps.model.LatLng(destination.latitude, destination.longitude))
-                    .await()
-                drawDetailedRoute(directionsResult)
-            } catch (e: Exception) {
-                // Handle exception
-                e.printStackTrace()
-            }
+        try {
+            return DirectionsApi.newRequest(context)
+                .mode(TravelMode.DRIVING)
+                .origin(com.google.maps.model.LatLng(origin.latitude, origin.longitude))
+                .destination(com.google.maps.model.LatLng(destination.latitude, destination.longitude))
+                .await()
+        } catch (e: Exception) {
+            // Gérer l'exception
+            e.printStackTrace()
+            // En cas d'erreur, retourner une instance vide de DirectionsResult
+            return DirectionsResult()
         }
     }
 
@@ -130,22 +135,75 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         val polylineOptions = PolylineOptions()
             .width(5f)
-            .color(Color.RED)
+            .color(Color.BLUE)
 
         for (step in leg.steps) {
             val points = step.polyline.decodePath()
-            val convertedPoints = ArrayList<LatLng>()
-
-            // Convertir les points de com.google.maps.model.LatLng à com.google.android.gms.maps.model.LatLng
             for (point in points) {
-                convertedPoints.add(LatLng(point.lat, point.lng))
+                val convertedPoint = LatLng(point.lat, point.lng)
+                polylineOptions.add(convertedPoint)
             }
-
-            polylineOptions.addAll(convertedPoints)
         }
 
-        polyline = googleMap.addPolyline(polylineOptions)
+        googleMap.addPolyline(polylineOptions)
     }
 
+    private fun decodePolyline(encodedPath: String): List<LatLng> {
+        val polyUtilPoints = PolyUtil.decode(encodedPath)
+        return polyUtilPoints.map { LatLng(it.latitude, it.longitude) }
+    }
+
+    private fun loadPointsFromJson(): List<LatLng> {
+        val points = ArrayList<LatLng>()
+        val jsonString = loadJsonFromAsset("route.json")
+
+        Log.d("JsonContent", jsonString)
+
+        try {
+            val jsonObject = JSONObject(jsonString)
+            val pointsArray = jsonObject.getJSONArray("points")
+
+            for (i in 0 until pointsArray.length()) {
+                val jsonPoint = pointsArray.getJSONObject(i)
+                val lat = jsonPoint.getDouble("lat")
+                val lng = jsonPoint.getDouble("lng")
+                points.add(LatLng(lat, lng))
+            }
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+        return points
+    }
+
+    private fun loadJsonFromAsset(fileName: String): String {
+        var json: String? = null
+        try {
+            val inputStream = assets.open(fileName)
+            val size = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer)
+            inputStream.close()
+            json = String(buffer, Charset.defaultCharset())
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            return ""
+        }
+
+        return json ?: ""
+    }
+
+    private fun addMarker(point: LatLng) {
+        val originalIcon = BitmapFactory.decodeResource(resources, R.drawable.trash_bin)
+        val resizedIcon = Bitmap.createScaledBitmap(originalIcon, 100, 100, false)
+        val customIcon = BitmapDescriptorFactory.fromBitmap(resizedIcon)
+
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(point)
+                .title("Marker Title")
+                .snippet("Marker Description")
+                .icon(customIcon)
+        )
+    }
 
 }
