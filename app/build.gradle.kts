5c496528342beb0255cd3fea4f2272894db250ad
pilou <pl.squaglia@gmail.com>
2023-12-21 09:36:53 +0100

    fix version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.hackathon_2023"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.hackathon_2023"
